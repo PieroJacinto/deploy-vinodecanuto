b6dfa49b32f31611d53cecc6b205f6b34377601c
Piero <pierojacinto@gmail.com>
2023-05-24 13:57:26 -0300

    express session en index

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 require("dotenv").config();
 const app = require("./src/app")
+//REQUERIMOPS EXPRESS
+const express = require("express");
+
+//REQUERIMOS SESSION
+const session = require('express-session');
+
+//EJECUTAMOS EXPRESS
+const app = express();
 app.use(session({
     resave: false,
     saveUninitialized: false,
