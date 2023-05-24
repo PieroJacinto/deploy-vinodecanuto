d8470c3e4114814b0869b3c1845f4efdf6b14a94
Piero <pierojacinto@gmail.com>
2023-05-24 13:56:04 -0300

    session en index

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const app = require("./src/app")
-
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'secret',       
+}));
 const port = process.env.PORT || 4000;
 
 app.listen(port);
