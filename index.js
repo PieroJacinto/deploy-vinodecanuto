49b1e3de181f2ea3d180396815e1599b8b78e14f
Piero <pierojacinto@gmail.com>
2023-05-24 13:59:00 -0300

    revert push

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 require("dotenv").config();
 const app = require("./src/app")
-//REQUERIMOPS EXPRESS
-const express = require("express");
 
-//REQUERIMOS SESSION
-const session = require('express-session');
-
-//EJECUTAMOS EXPRESS
-const app = express();
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: 'secret',       
-}));
 const port = process.env.PORT || 4000;
 
 app.listen(port);
